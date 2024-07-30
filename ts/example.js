bd35fe016e846b076a278794734f5d9e8f9bd0cb
talpaa <daniele.taccini@gmail.com>
2024-07-30 14:00:33 +0200

    m

---
+++ ts/example.js
@@ -0,0 +1,7 @@
+function Somma(x, y) {
+    return x + y;
+}
+var x = 2;
+var y = 7;
+var somma = Somma(x, y);
+console.log("La somma vale " + somma);
---
+++ ts/example.ts
@@ -0,0 +1,12 @@
+function Somma(x: number, y: number): number 
+{
+    
+    return x + y;
+}
+
+var x: number = 2
+var y: number = 7
+  
+var somma:number = Somma(x, y);
+console.log("La somma vale " + somma)
+  
